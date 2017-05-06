6c17590ed4cc7127ea455f1180daabf01e8dba4c
zhouwenkai <zhouwenkai@baidu.com>
2017-05-06 12:05:09 +0800

    Init project

---
+++ core/src/main/java/com/kevin/rxbus/annotation/Subscribe.java
@@ -0,0 +1,18 @@
+package com.kevin.rxbus.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by zhouwenkai on 2017/5/6.
+ * 2017-05-06 11:02:44
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface Subscribe {
+    SchedulerType scheduler() default SchedulerType.TRAMPOLINE;
+}
--- samples/src/main/java/com/kevin/rxbus/samples/MainActivity.java
+++ samples/src/main/java/com/kevin/rxbus/samples/MainActivity.java
@@ -1,4 +1,4 @@
-package com.kevin.rxbus;
+package com.kevin.rxbus.samples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
